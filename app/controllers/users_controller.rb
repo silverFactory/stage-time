23a7d95df658358e2c66544c5e9c106d32d94874
Will Studabaker <will.studabaker@gmail.com>
2021-01-28 12:09:53 -0500

    user show pic & name situated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    byebug
+    #byebug
     @user.save
     redirect_to user_path(@user)
   end
