0f1a0702ff9c87e49fe16b5ee8077038198c8ccf
Will Studabaker <will.studabaker@gmail.com>
2021-01-30 11:02:17 -0500

    support validations added

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -6,20 +6,31 @@ class SupportsController < ApplicationController
     @support = Support.new
     @missed_connection = true
     @lend_a_hand = false
+    @title = "Post a Missed Connection"
     render "new"
   end
   def new_lend_a_hand
     @support = Support.new
     @lend_a_hand = true
     @missed_connection = false
+    @title = "Ask For Help This Week"
     render "new"
   end
   def create
     @support = Support.new(support_params)
     @support.user_id = session[:user_id]
     @support.open_mic_id = params[:open_mic_id]
-    @support.save
-    redirect_to open_mic_support_path(params[:open_mic_id], @support)
+    if @support.valid?
+      @support.save
+      redirect_to open_mic_support_path(params[:open_mic_id], @support)
+    else
+      if @support.missed_connection == true
+        @title = "Post a Missed Connection"
+      else
+        @title = "Ask For Help This Week"
+      end
+      render "new"
+    end
   end
   def show
     @support = Support.find(params[:id])
--- app/models/support.rb
+++ app/models/support.rb
@@ -1,4 +1,7 @@
 class Support < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+  
   belongs_to :open_mic
   belongs_to :user
   has_many :comments
