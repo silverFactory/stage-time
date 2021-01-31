9fd767af52001dfb7812b7d3f5e76189ba0583fb
Will Studabaker <will.studabaker@gmail.com>
2021-01-30 19:47:36 -0500

    Edit Profile button added to navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,24 +4,24 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    if auth['uid'] != nil
-      @user = User.find_by(uid: auth['uid'])
-       #if there is an existing user with uid: auth['uid']
-      if @user != nil
-        session[:user_id] = @user.id
-        redirect_to user_path(session[:user_id])
-      else #starts a new user and redirect them to edit user page to fill in rest of info
-        @user = User.new(uid: auth['uid'])
-        render "users/edit"
-        if @user.valid?
-          @user.save
-          session[:user_id] = @user.id
-          redirect_to user_path(@user)
-        else
-          render "users/edit"
-        end
-      end
-    else #non-oauth login
+    # if auth['uid'] != nil
+    #   @user = User.find_by(uid: auth['uid'])
+    #    #if there is an existing user with uid: auth['uid']
+    #   if @user != nil
+    #     session[:user_id] = @user.id
+    #     redirect_to user_path(session[:user_id])
+    #   else #starts a new user and redirect them to edit user page to fill in rest of info
+    #     @user = User.new(uid: auth['uid'])
+    #     render "users/edit"
+    #     if @user.valid?
+    #       @user.save
+    #       session[:user_id] = @user.id
+    #       redirect_to user_path(@user)
+    #     else
+    #       render "users/edit"
+    #     end
+    #   end
+    # else #non-oauth login
         @user = User.find_by(username: params[:username])
         if @user == nil
           redirect_to signin_path, notice: "Invalid username."
@@ -31,7 +31,7 @@ class SessionsController < ApplicationController
         else
           redirect_to signin_path, notice: "Invalid password."
         end
-    end
+    # end
   end
   def destroy
     session[:user_id] = nil
