645b43e98ddc75de245f647c238855fa6e491544
Will Studabaker <will.studabaker@gmail.com>
2021-01-30 20:55:31 -0500

    update button added to navbar

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -4,10 +4,9 @@ class UpdatesController < ApplicationController
   end
   def create
     @update = Update.new(update_params)
-    @update.user_id = session[:user_id]
     @update.open_mic_id = params[:open_mic_id]
     @update.save
-    redirect_to open_mic_show(params[:open_mic_id])
+    redirect_to open_mic_path(params[:open_mic_id])
   end
   def edit
   end
