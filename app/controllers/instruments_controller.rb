db136bb28067a8be4f85d291dfed5d40c52fdcef
Will Studabaker <will.studabaker@gmail.com>
2021-01-31 09:45:12 -0500

    instruments index added, host can add and edit instruments

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -2,6 +2,10 @@ class InstrumentsController < ApplicationController
   def new
     @instrument = Instrument.new
   end
+  def index
+    @open_mic = OpenMic.find(params[:open_mic_id])
+    @instruments = @open_mic.instruments
+  end
   def create
     @instrument = Instrument.new(instrument_params)
     @instrument.open_mic_id = params[:open_mic_id]
@@ -10,14 +14,18 @@ class InstrumentsController < ApplicationController
       redirect_to open_mic_instrument_path(params[:open_mic_id], @instrument)
     else
       render :new
-    end 
+    end
   end
   def show
     @instrument = Instrument.find(params[:id])
   end
   def edit
+    @instrument = Instrument.find(params[:id])
   end
   def update
+    @instrument = Instrument.find(params[:id])
+    @instrument.update(instrument_params)
+    redirect_to open_mic_instrument_path(params[:open_mic_id], @instrument)
   end
   def destroy
   end
